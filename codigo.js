7cebeb154dd7b35dbfb23ebebaf67561e305bfa8
ascenciovil <jascencio21@alumnos.utalca.cl>
2023-07-17 18:43:06 -0400

    Celda clickeable pero no desaparece

--- codigo.js
+++ codigo.js
@@ -1,64 +1,66 @@
 function GenerarF(){
-    alert("Usted Seleccionado el Nivel Facil : ");
-    var tabla="<table border=\"0\">";
-    for(i=0;i<9;i++){
-    tabla+="<tr>";
-    for(j=0;j<9;j++){
-    tabla+="<td>"+"<a href=\"#\""+"<IMG src=\"Bloque.ico\">"+"</a>" +"</td>";
-    }
-    tabla+="</tr>";
-    }
-    tabla+="</table>";
-    document.getElementById("Resultado").innerHTML=tabla;
-    
-  };
+  alert("Usted Seleccionado el Nivel Facil : ");
+  var tabla="<table border=\"0\">";
+  for(i=0;i<9;i++){
+  tabla+="<tr>";
+  for(j=0;j<9;j++){
+  tabla+="<td>"+"<a href=\"#\" onClick = \"disapear();\">"+"<IMG src=\"Bloque.ico\" id = \"block\">"+"</a>" +"</td>";
+  }
+  tabla+="</tr>";
+  }
+  tabla+="</table>";
+  document.getElementById("Resultado").innerHTML=tabla;
   
-  function GenerarM(){
-    alert("Usted Seleccionado el Nivel Medio : ");
-    var tabla="<table border=\"0\">";
-    for(i=0;i<16;i++){
-    tabla+="<tr>";
-    for(j=0;j<16;j++){
-    tabla+="<td>"+"<a href=\"#\""+"<IMG src=\"Bloque.ico\">"+"</a>" +"</td>";
-    }
-    tabla+="</tr>";
-    }
-    tabla+="</table>";
-    document.getElementById("Resultado").innerHTML=tabla;
-    
-  };
+};
+
+function GenerarM(){
+  alert("Usted Seleccionado el Nivel Medio : ");
+  var tabla="<table border=\"0\">";
+  for(i=0;i<16;i++){
+  tabla+="<tr>";
+  for(j=0;j<16;j++){
+    tabla+="<td>"+"<a href=\"#\" onClick = \"disapear();\">"+"<IMG src=\"Bloque.ico\" id = \"block\">"+"</a>" +"</td>";
+  }
+  tabla+="</tr>";
+  }
+  tabla+="</table>";
+  document.getElementById("Resultado").innerHTML=tabla;
   
-  function GenerarD(){
-    alert("Usted Seleccionado el Nivel Dificil : ");
-    var tabla="<table border=\"0\">";
-    for(i=0;i<32;i++){
-    tabla+="<tr>";
-    for(j=0;j<32;j++){
-    tabla+="<td>"+"<a href=\"#\""+"<IMG src=\"Bloque.ico\">"+"</a>" +"</td>";
-    }
-    tabla+="</tr>";
-    }
-    tabla+="</table>";
-    document.getElementById("Resultado").innerHTML=tabla;
-    
-  };
+};
+function disapear(id){
+  document.getElementById(id).style.opacity = "0";
+}
+function GenerarD(){
+  alert("Usted Seleccionado el Nivel Dificil : ");
+  var tabla="<table border=\"0\">";
+  for(i=0;i<32;i++){
+  tabla+="<tr>";
+  for(j=0;j<32;j++){
+    tabla+="<td>"+"<a href=\"#\" onClick = \"disapear();\">"+"<IMG src=\"Bloque.ico\" id = \"block\">"+"</a>" +"</td>";
+  }
+  tabla+="</tr>";
+  }
+  tabla+="</table>";
+  document.getElementById("Resultado").innerHTML=tabla;
   
-  
-  function CrearTabla(){
-  
-    var Op1=document.getElementById("Facil");
-    var Op2=document.getElementById("Medio");
-    var Op3=document.getElementById("Dificil");
-  
-  
-    if (Op1.checked==true){
-    GenerarF();
-    }
-    if (Op2.checked==true){
-    GenerarM();
-    } 
-    if (Op3.checked==true){
-    GenerarD();
-    }
-     
-  };
+};
+
+
+function CrearTabla(){
+
+  var Op1=document.getElementById("Facil");
+  var Op2=document.getElementById("Medio");
+  var Op3=document.getElementById("Dificil");
+
+
+  if (Op1.checked==true){
+  GenerarF();
+  }
+  if (Op2.checked==true){
+  GenerarM();
+  } 
+  if (Op3.checked==true){
+  GenerarD();
+  }
+   
+};
